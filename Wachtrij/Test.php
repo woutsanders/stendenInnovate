6d64401209afd7a30e84a796a2f0b859bfc0f18b
rbeikes <memyselfandi105@hotmail.com>
2015-05-11 11:08:02 +0200

    Eerste paar wachtrij checks toegevoegd

--- Wachtrij/Test.php
+++ Wachtrij/Test.php
@@ -1,14 +1,44 @@
 <?php
 require('connect.php');
 function gameCheck()
-{
-$query = "SELECT Gebruikersnaam, tijd FROM speler WHERE Status = 'Wachtrij' ORDER BY tijd ASC";
-$result = mysql_query($query) or die(mysql_error());
-while ($rows = mysql_fetch_array ($result))
-{
-echo ($rows[0]);
-    echo ($rows[1]);
-}
+{   //Query, checkt of er mensen zijn die aan het spelen zijn
+    $queryCheck = "SELECT Gebruikersnaam FROM speler WHERE Status = 'playing'";
+    $resultCheck = mysql_query($queryCheck) or die(mysql_error());
+    if ($resultCheck)
+    {
+        echo "Er zijn nog mensen aan het spelen, één moment geduld";
+    }
+    else
+    {   //Query, zijn er nog mensen waarvan wij wachten op ready signaal?
+        $queryWaiting = "SELECT Gebruikersnaam FROM speler WHERE Status = 'waiting for ready'";
+        $resultWaiting = mysql_query($queryWaiting) or die(mysql_error());
+        if($resultWaiting)
+        {
+            //Koppelen van gebruiker aan gebruikersnaam die aan het wachten is
+            //Status van gebruiker wijzigen in 'waiting for ready'
+            //Query voor inserten, gebruikersnaam = gebruikersnaam van de persoon die iets invuld
+            $queryInsert = "INSERT INTO speler (Status) VALUES ('waiting for ready') WHERE Gebruikersnaam = 'xxx'";
+        }
+        else
+        {   //Query, laat alleen de langst wachtende speler zien
+            $query = "SELECT Gebruikersnaam FROM speler WHERE Status = 'waiting' ORDER BY tijd ASC LIMIT 1";
+            $result = mysql_query($query) or die(mysql_error());
+            if ($result)
+            {
+                while ($rows = mysql_fetch_array($result))
+                {
+                    echo($rows[0]);
+                    echo "<br>";
+
+                    //Status van beide spelers wijzigen naar 'waiting for ready'
+                }
+            }
+            else
+            {
+                echo "Er is nog geen tegenspeler, één moment geduld";
+            }
+        }
+    }
 }
 if(isset($_POST['test']))
 {
--- Wachtrij/username.php
+++ Wachtrij/username.php
@@ -15,8 +15,46 @@ function newUser()
     $time = time();
     $queryInsert = "INSERT INTO speler (Gebruikersnaam, tijd) VALUES ('$userName', $time)";
     $data = mysql_query ($queryInsert)or die(mysql_error());
-    if ($data) {
-        echo "YOUR REGISTRATION IS COMPLETED!";
+    if ($data)
+    {
+        echo "U wordt in de wachtrij geplaatst, één moment geduld alstublieft.";
+        $queryCheck = "SELECT Gebruikersnaam FROM speler WHERE Status = 'playing'";
+        $resultCheck = mysql_query($queryCheck) or die(mysql_error());
+        if ($resultCheck)
+        {
+            echo "Er zijn nog mensen aan het spelen, één moment geduld";
+        }
+        else
+        {   //Query, zijn er nog mensen waarvan wij wachten op ready signaal?
+            $queryWaiting = "SELECT Gebruikersnaam FROM speler WHERE Status = 'waiting for ready'";
+            $resultWaiting = mysql_query($queryWaiting) or die(mysql_error());
+            if($resultWaiting)
+            {
+                //Koppelen van gebruiker aan gebruikersnaam die aan het wachten is
+                //Status van gebruiker wijzigen in 'waiting for ready'
+                //Query voor inserten, gebruikersnaam = gebruikersnaam van de persoon die iets invuld
+                $queryInsert = "INSERT INTO speler (Status) VALUES ('waiting for ready') WHERE Gebruikersnaam = $userName";
+            }
+            else
+            {   //Query, laat alleen de langst wachtende speler zien
+                $query = "SELECT Gebruikersnaam FROM speler WHERE Status = 'waiting' ORDER BY tijd ASC LIMIT 1";
+                $result = mysql_query($query) or die(mysql_error());
+                if ($result)
+                {
+                    while ($rows = mysql_fetch_array($result))
+                    {
+                        echo($rows[0]);
+                        echo "<br>";
+
+                        //Status van beide spelers wijzigen naar 'waiting for ready'
+                    }
+                }
+                else
+                {
+                    echo "Er is nog geen tegenspeler, één moment geduld";
+                }
+            }
+        }
     }
 }
 // signUp bevat alle checks, de newUser functie is hierin genest
