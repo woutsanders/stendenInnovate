221448ca4e059726ff9784f59747609f1ccba08f
JeroenM <joen10@live.nl>
2015-05-12 18:28:47 +0200

    UPDATE aangepast voor het ajax/jquery verhaal

--- backend_php/bootstrap.php
+++ backend_php/bootstrap.php
@@ -17,8 +17,8 @@ require_once('autoload.php');
 $server = "localhost";
 $userName = "root";
 $passWord = "";
-//$DBName = "project_innovate";
-$DBName = "hallo";
+$DBName = "project_innovate";
+//$DBName = "hallo";
 
 //verbind met de DBMS
 $connection = mysqli_connect($server, $userName, $passWord, $DBName) OR DIE (mysqli_error($connection));
--- backend_php/username.php
+++ backend_php/username.php
@@ -12,17 +12,14 @@ require_once('bootstrap.php');
 // signUp bevat alle checks, de newUser functie is hierin genest
 $data = 0;
 $username = 0;
-if(isset($_POST['submit']))
-{
-    $username = $_POST['user'];
+    $username = $_POST['forminput'];
+
     $data = signUp($connection,$username);
-
-}
-
-
+echo $data;
     if ($data)
     {
         $resultcheck = playingGameCheck($connection);
+        echo $resultcheck;
         if ($resultcheck == false)
         {
             echo "Er zijn nog mensen aan het spelen, één moment geduld";
@@ -30,13 +27,15 @@ if(isset($_POST['submit']))
         else
         {   //Query, zijn er nog mensen waarvan wij wachten op ready signaal?
            $resultWaiting = checkReadySignal($connection);
+            echo $resultWaiting;
             if ($resultWaiting == true)
             {
                 //Koppelen van gebruiker aan gebruikersnaam die aan het wachten is
                 //Status van gebruiker wijzigen in 'waiting for ready'
                 //Query voor inserten, gebruikersnaam = gebruikersnaam van de persoon die iets invuld
-                $queryInsertStatus = "INSERT INTO speler (Status) VALUES ('waiting for ready') WHERE Gebruikersnaam = $userName";
-                $queryUitvoeren = mysqli_query($connection , $queryInsertStatus) or die(mysql_error());
+                $status = "waiting for ready";
+                $queryUitvoeren = changeStatus($connection, $status, $username);
+                echo $queryUitvoeren;
                 if ($queryUitvoeren)
                 {
                     //Ready knop tonen
@@ -49,17 +48,19 @@ if(isset($_POST['submit']))
             else
             {
                 $result = longestWaiting($connection);
+                echo $result;
                 if ($result)
                 {
-                    while ($rows = mysql_fetch_array($result))
+                    while ($rows = mysqli_fetch_array($result))
                     {
                         echo($rows[0]);
                         echo "<br>";
 
                         //Status van speler wijzigen naar 'waiting for ready'
                         $status = "waiting for ready";
-                        $queryUitvoeren2 = changeStatus($connection , $status, $username);
-                        if ($queryUitvoeren2)
+                        $queryUitvoeren = changeStatus($connection , $status, $username);
+                        echo $queryUitvoeren;
+                        if ($queryUitvoeren)
                         {
                             //Ready knop tonen
                         }
@@ -80,21 +81,3 @@ if(isset($_POST['submit']))
 
 ?>
 
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-<head>
-    <title>Wachtrij test</title>
-<body>
-<form action="username.php" method="post">
-    <table>
-        <tr>
-            <td>Username:</td>
-            <td><input type="text" name="user" /></td>
-        </tr>
-        <input type="submit" name="submit" value="Submit">
-</form>
-<form action="#" method="post">
-    <input type="submit" name="test" value="test">
-</body>
-</head>
-</html>
