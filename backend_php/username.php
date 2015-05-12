2f40834bb7ee9b31b1e26c8f51cf47d8559105aa
JeroenM <joen10@live.nl>
2015-05-12 09:52:29 +0200

    UPDATE, geupdatet aan de hand van de functions

--- backend_php/username.php
+++ backend_php/username.php
@@ -8,27 +8,27 @@
 require_once('bootstrap.php');
 
 
-// newUser word uitgevoerd wanneer alle checks gedaan zijn
-function newUser()
+
+// signUp bevat alle checks, de newUser functie is hierin genest
+$data = 0;
+$username = $_POST['user'];
+if(isset($_POST['submit']))
 {
-    $userName = $_POST['user'];
-    $time = time();
-    $queryInsert = "INSERT INTO speler (UserName, Date+Time, Status) VALUES ($userName, $time, 'waiting')";
-    $data = mysqli_query ($connection , $queryInsert)or die(mysqli_error($data));
+    $data = signUp($connection,$_POST['user']);
+}
+
+
     if ($data)
     {
-        echo "U wordt in de wachtrij geplaatst, één moment geduld alstublieft.";
-        $queryCheck = "SELECT Gebruikersnaam FROM speler WHERE Status = 'playing'";
-        $resultCheck = mysql_query($queryCheck) or die(mysql_error());
-        if ($resultCheck)
+        $resultcheck = playingGameCheck($connection);
+        if ($resultcheck == false)
         {
             echo "Er zijn nog mensen aan het spelen, één moment geduld";
         }
         else
         {   //Query, zijn er nog mensen waarvan wij wachten op ready signaal?
-            $queryWaiting = "SELECT Gebruikersnaam FROM speler WHERE Status = 'waiting for ready'";
-            $resultWaiting = mysql_query($queryWaiting) or die(mysql_error());
-            if ($resultWaiting)
+           $resultWaiting = checkReadySignal($connection);
+            if ($resultWaiting == true)
             {
                 //Koppelen van gebruiker aan gebruikersnaam die aan het wachten is
                 //Status van gebruiker wijzigen in 'waiting for ready'
@@ -45,9 +45,8 @@ function newUser()
                 }
             }
             else
-            {   //Query, laat alleen de langst wachtende speler zien
-                $query = "SELECT Gebruikersnaam FROM speler WHERE Status = 'waiting' ORDER BY tijd ASC LIMIT 1";
-                $result = mysql_query($query) or die(mysql_error());
+            {
+                $result = longestWaiting($connection);
                 if ($result)
                 {
                     while ($rows = mysql_fetch_array($result))
@@ -55,9 +54,8 @@ function newUser()
                         echo($rows[0]);
                         echo "<br>";
 
-                        //Status van beide spelers wijzigen naar 'waiting for ready'
-                        $queryInsertStatus2 = "INSERT INTO speler (Status) VALUES ('waiting for ready') WHERE Gebruikersnaam = $userName";
-                        $queryUitvoeren2 = mysql_query($queryInsertStatus2) or die(mysql_error());
+                        //Status van speler wijzigen naar 'waiting for ready'
+                        changeStatus($connection , $status, $username);
                         if ($queryUitvoeren2)
                         {
                             //Ready knop tonen
@@ -74,35 +72,7 @@ function newUser()
                 }
             }
         }
-    }
+
 }
-// signUp bevat alle checks, de newUser functie is hierin genest
-function signUp()
-{
-    $user = $_POST['user'];
-//preg_match forceert dat alle characters moeten bestaan uit a-z, A-Z en 0-9
-    if (preg_match("#^[a-zA-Z0-9]+$#", $user))
-    {
-//Checkt DB of de username al bestaat
-        if (!empty($user)) {
-            $query = mysql_query("SELECT * FROM speler WHERE Gebruikersnaam = '$user'") or die(mysql_error());
-            $row = mysql_fetch_array($query);
-//Wanneer er niks terug komt uit de query voer dan newUser uit
-            if ($row == false) {
-                newUser();
-            } else {
-                echo "Sorry, this username already exists!";
-            }
-        } else {
-            echo "Please fill in a username!";
-        }
-    }
-    else {
-        echo "Invalid characters!";
-    }
-}
-if(isset($_POST['submit']))
-{
-    signUp();
-}
+
 ?>
