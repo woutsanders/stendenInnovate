7ea62d89770070baa1811433c74d9b4ea395bd85
Wout Sanders <wsanders@outlook.com>
2015-05-12 15:11:34 +0200

    SF2 backend: Added unity controller

---
+++ backend/src/CurveGame/ApiBundle/Controller/UnityController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace CurveGame\ApiBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class UnityController
+ * @package CurveGame\ApiBundle\Controller
+ */
+class UnityController extends BaseController {
+
+
+    public function userDataAction() {
+
+
+    }
+
+    public function commandAction(Request $request, $userId) {
+
+
+    }
+}
