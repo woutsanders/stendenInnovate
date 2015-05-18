0fdbb9e84ffc59308193d4b4648d52bce0b22dc8
Wout Sanders <wsanders@outlook.com>
2015-05-18 13:01:36 +0200

    SF2 backend: Added a comment

--- backend/src/CurveGame/WebBundle/Controller/IndexController.php
+++ backend/src/CurveGame/WebBundle/Controller/IndexController.php
@@ -6,6 +6,11 @@ use \Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class IndexController extends Controller {
 
+    /**
+     * Displays the index page.
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function indexAction() {
 
         return $this->render('CurveGameWebBundle:Index:index.html.twig');
