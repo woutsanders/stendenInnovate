5bdb04b74b306b5b0361f336da6b3111741ba61b
Wout Sanders <wsanders@outlook.com>
2015-05-21 12:09:20 +0200

    SF2 backend: Finished the poll action to check when player is on turn.

--- backend/src/CurveGame/ApiBundle/Controller/QueueController.php
+++ backend/src/CurveGame/ApiBundle/Controller/QueueController.php
@@ -7,11 +7,50 @@ use Symfony\Component\HttpFoundation\Request;
 
 class QueueController extends BaseController {
 
+    /**
+     * Returns true if user is on turn, this way AJAX can respond to this event.
+     *
+     * @param null $userId
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws ApiException
+     */
     public function pollAction($userId = null) {
 
         // Checks if user is on turn (true / false) and set status to waiting for ready if true.
+        $em = $this->getDoctrine()->getManager();
+        $playerRepo = $em->getRepository('CurveGameEntityBundle:Player');
+        $statusRepo = $em->getRepository('CurveGameEntityBundle:Player');
 
+        $pos = $playerRepo->findPositionInQueue($userId);
 
+        if ($pos === false) {
+
+            throw new ApiException(ApiException::HTTP_BAD_REQUEST, "Invalid userID specified");
+        }
+
+        if ((int) $pos === 1) {
+
+            $player = $playerRepo->findOneBy(array(
+                'id'    => $userId,
+            ));
+
+            $player->setStatus($statusRepo->findOneByStatusName('waiting for ready'));
+            $player->setTimestamp(time());
+            $em->flush();
+
+            $respArr = array(
+                'onTurn'    => true,
+            );
+
+            return $this->jsonResponse($respArr);
+        } else {
+
+            $respArr = array(
+                'onTurn'    => false,
+            );
+
+            return $this->jsonResponse($respArr);
+        }
     }
 
     public function confirmReadyAction(Request $request, $userId) {
