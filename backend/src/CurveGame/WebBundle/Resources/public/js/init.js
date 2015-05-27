ffd42171d3da5e7bdddf1b035e86cfcc3524e723
Wout Sanders <wsanders@outlook.com>
2015-05-27 08:39:44 +0200

    SF2 backend: Added some JS files

---
+++ backend/src/CurveGame/WebBundle/Resources/public/js/init.js
@@ -0,0 +1,10 @@
+/**
+ * Initializing variables to use during this session.
+ */
+var userId = 0;
+var leftControl = 1;
+var straightControl = 0;
+var rightControl = 2;
+var keyLeft = 37;
+var keyRight = 39;
+var keyDown = false;
