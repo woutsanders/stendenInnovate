816123f96aa6a86b920bd91bfbd4f5b3271e3415
Wout Sanders <wsanders@outlook.com>
2015-06-04 18:53:41 +0200

    SF2 backend: Fixed websocket bugs

--- backend/src/CurveGame/WebBundle/Resources/public/js/15_websocket.js
+++ backend/src/CurveGame/WebBundle/Resources/public/js/15_websocket.js
@@ -7,13 +7,29 @@ var ws = {
     socket: undefined,
     supported: undefined,
     checkSupport: function() {
-        return (!this.supported ? ((window.WebSocket || (typeof(WebSocket) == "object")) ? true : false) : this.supported);
+        if (!ws.supported) {
+            if (window.WebSocket || (typeof(WebSocket) == "object")) {
+                ws.supported = true;
+                return true;
+            } else {
+                ws.supported = false;
+                return false;
+            }
+        }
     },
     connect: function() {
+        if (!this.supported)
+            return;
         if (debug)
             console.log("Attempting WS connect()... ws.connect()");
-        this.socket = new WebSocket("ws://" + this.url + ":" + this.port + "/curvegame");
-        if (this.socket) {
+
+        ws.socket = new WebSocket("ws://" + this.url + ":" + this.port + "/curvegame");
+        ws.onmessage = function(data) {
+            if (debug)
+                console.log("ws.onmessage(): Received: --data: " + data);
+        };
+
+        if (ws.socket) {
             if (debug)
                 console.log("Connected with server in ws.connect()");
         } else {
@@ -22,27 +38,18 @@ var ws = {
         }
     },
     sendUnityCtrlCmd: function(msg) {
-        if (this.socket != null) {
+        if (ws.socket != undefined) {
             var data = JSON.stringify(msg);
             if (debug)
-                console.log("Initiating... ws.sendUnityCtrlCmd(msg): --msg: " + data);
-            if(this.socket.send(JSON.stringify(data))) {
-                if (debug)
-                    console.log("WebSocket: Data sent in ws.sendUnityCtrlCmd(): --msg: " + data);
-            } else {
-                if (debug)
-                    console.log("Error: WebSocket could not sent data to server in ws.sendUnityCtrlCmd()");
-            }
+                console.log("WebSocket: Data sent in ws.sendUnityCtrlCmd(): --msg: " + data);
+            ws.socket.send(data);
         }
     },
     close: function() {
-        if (this.socket != null)
-            if (this.socket.close()) {
-                if (debug)
-                    console.log("WebSocket: Closed connection explicitly in ws.close()");
-            } else {
-                if (debug)
-                    console.log("Error: WebSocket could not close connection in ws.close()");
-            }
+        if (this.socket != undefined) {
+            this.socket.close();
+            if (debug)
+                console.log("WebSocket: Closed connection explicitly in ws.close()");
+        }
     }
 };
