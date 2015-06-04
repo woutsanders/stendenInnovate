da6ac5d64bdca4022b3ef20601d983e9e4dcb597
Wout Sanders <wsanders@outlook.com>
2015-06-04 18:55:32 +0200

    SF2 backend: Rewritten the Unity server to not eat CPU (used tornado.tcpserver as better alternative)

--- backend/bin/server.py
+++ backend/bin/server.py
@@ -1,35 +1,62 @@
+import json
+import sys
+import signal
+import time
 import tornado.web
 import tornado.websocket
 import tornado.httpserver
-import tornado.ioloop
-import json
-import SocketServer
-import socket
-import sys
-import threading
-import signal
-import time
 from collections import namedtuple
+from tornado.tcpserver import TCPServer
+from tornado.ioloop import IOLoop
+
 
 # Define some global variables.
 global pMsg
-global t1
-global allowSend
-allowSend = True
+
 
 # Initialize server app...
 def main():
+    global pMsg;
+    pMsg = ""
+    
+    signal.signal(signal.SIGINT, exitApplication)
+    signal.signal(signal.SIGTERM, exitApplication)
+    
     ws_app = Application()
-    server = tornado.httpserver.HTTPServer(ws_app)
-    server.bind(9989)
-    server.start()
+    ws_srv = tornado.httpserver.HTTPServer(ws_app)
+    ws_srv.bind(9989)
+    ws_srv.start()
     print "[*] WebSockets server [*] UP"
     
-    server1 = SocketServer.TCPServer(("", 9990), HandleUnityTCP)
-    t1 = threading.Thread(target=server1.serve_forever)
-    t1.start()
+    u_srv = UnityServer()
+    u_srv.listen(9990)
     print "[*] Unity server      [*] UP"
-    tornado.ioloop.IOLoop.instance().start()
+    
+    IOLoop.instance().start()
+    IOLoop.instance().close()
+
+
+# Helper to properly exit the servers.
+def exitApplication(sig, frame):
+    IOLoop.instance().add_callback(IOLoop.instance().stop)
+
+
+# The Unity server
+class UnityServer(TCPServer):
+    
+    def handle_stream(self, stream, address):
+        global pMsg
+        self._stream = stream
+        self._handle_read(pMsg)
+        #self._read_line()
+    
+    #def _read_line(self):
+        #self._stream.read_until('\n', self._handle_read)
+    
+    def _handle_read(self, data):
+        print "UnityServer: Writing back: " + str(data)
+        self._stream.write(data)
+        #self._read_line()
 
 
 # Initializes the WebSockets server.
@@ -37,7 +64,7 @@ class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
                     (r'/curvegame', WebSocketHandler)
-        ]
+                    ]
         tornado.web.Application.__init__(self, handlers)
 
 
@@ -45,47 +72,28 @@ class Application(tornado.web.Application):
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
     def open(self):
         pass
- 
+    
     def on_message(self, json):
         global pMsg
         global allowSend
         
-    	msgDecoded = self.jsonDecode(json)
-    	
-    	userID = msgDecoded.userId
-        direction = msgDecoded.direction
+        msgDecoded = self.jsonDecode(json)
         
-        print "Received ID: " + str(userID)
-    	print "Movement digit: " + str(direction)
-        
+        userID = msgDecoded.userId
+        direction = msgDecoded.moveTo
         pMsg = str(userID) + ":" + str(direction)
+        print "WebSocketHandler: Received: " + pMsg
         self.write_message(u"OK")
- 
+    
     def on_close(self):
         pass
-
+    
     # Disable check-origin header...
     def check_origin(self, origin):
-        return True 
+        return True
     
     def jsonDecode(self, message):
-    	return json.loads(message, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
-
-
-# Unity TCP Server
-class HandleUnityTCP(SocketServer.BaseRequestHandler):
-    def handle(self):
-        global allowSend
-        global pMsg
-        
-        while True:
-            if (allowSend):
-                try:
-                    self.request.sendall(pMsg + "\r\n")
-                    time.sleep(0.003)
-                except:
-                    print "HandleUnityTCP: not sent"
-                    time.sleep(0.005)
+        return json.loads(message, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
 
 # Handle main function on startup...
 if __name__ == '__main__':
