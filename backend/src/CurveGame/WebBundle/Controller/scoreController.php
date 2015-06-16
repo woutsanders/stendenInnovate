4f0a3f370384f7440fa723b5f92e19b3ccb45fb4
Wout Sanders <wsanders@outlook.com>
2015-06-16 11:44:51 +0200

    SF2 backend: Finished score method

--- backend/src/CurveGame/WebBundle/Controller/scoreController.php
+++ backend/src/CurveGame/WebBundle/Controller/scoreController.php
@@ -6,25 +6,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ScoreController extends Controller {
 
+    /**
+     * Shows a page with the ten players with the highest scores of the current day.
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function indexAction() {
-
-        //Bepalen welke dag het vandaag is, zodat alleen de score van vandaag geshowed wordt
-        $dt = new \DateTime(time());
-        $day = $dt->format('Y-m-d');
-        $startTime = strtotime($day);
-        $dayTime = 86400;
-        $endTime = $startTime + $dayTime;
 
         //custom query voor het ophalen van de score
         $em = $this->getDoctrine()->getManager();
-        $query = $em->CreateQuery(
-            "SELECT p
-                FROM CurveGameEntityBundle:Highscore p
-                WHERE p.DateTime > $startTime AND p.DateTime < $endTime"
-        );
-        $highscore = $query->getResult();
+        $playerRepo = $em->getRepository('CurveGameEntityBundle:Player');
+        $players = $playerRepo->findByTopTenScore();
 
-        //Resultaat query als array naar view omgeving doorgeven
-        return $this->render('default/highscore.html.twig', array('Users' => $highscore));
+        return $this->render('default/highscore.html.twig', array(
+            'Users' => $players,
+        ));
     }
 }
