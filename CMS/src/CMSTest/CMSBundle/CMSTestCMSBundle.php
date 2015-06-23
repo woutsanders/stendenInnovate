30d612aa0c31a71bab65ecf1d3efc0a4441a089d
JeroenM <joen10@live.nl>
2015-06-23 20:34:21 +0200

    SF2 backend, Removed testing environment

--- CMS/src/CMSTest/CMSBundle/CMSTestCMSBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace CMSTest\CMSBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class CMSTestCMSBundle extends Bundle
-{
-}
--- CMS/src/Pietje/JantjeBundle/PietjeJantjeBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Pietje\JantjeBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class PietjeJantjeBundle extends Bundle
-{
-}
